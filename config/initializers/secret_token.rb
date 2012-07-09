018b0fe6a6567072ac86f5b1955132bc88f83790
Tensho <andrew.babichev@gmail.com>
2012-07-09 16:01:41 +0300

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'a8d59d0bd263f06d00a7be87579e56b59f6c536ce2d2831307fdf2b3907b6d3ca031b0e1d86ead97c259afd781ddf1b4950dead57ebf8260dbba10ab0b41d6a6'
---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create(title: 'Soft Kitty',
+	description: %{<p>Soft kitty, Warm kitty, Little ball of fur. Happy kitty, Sleepy kitty, Purr, purr, purr.</p>},
+	image_url: 'soft_kitty.jpg',
+	price: 150.00)
+Product.create(title: 'Sleepy Angel',
+	description: %{<p>I am sleepy ange... Z-z-z, z-z-z.</p>},
+	image_url: 'sleepy_angel.jpg',
+	price: 160.00)
+Product.create(title: 'Famous Giraffe',
+	description: %{<p>This is a very famous giraffe Garold.</p>},
+	image_url: 'famous_giraffe.jpg',
+	price: 170.00)
