92d7db422f9363c0a4c74c284df049b796b007d9
Tensho <andrew.babichev@gmail.com>
2012-07-11 16:38:53 +0300

    Validation

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :products
+
+	test "product attributes must not be empty" do
+		# свойства товара не должны оставаться пустыми
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:description].any?
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+	end
+
+	test "product price must be positive" do
+		# цена товара должна быть положительной
+		product = Product.new(title: 'Lorem Ipsum',
+													description: 'Lorem Ipsum Description',
+													image_url: 'lorem.jpg')
+		product.price = -1
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",	product.errors[:price].join('; ')
+		product.price = 0
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",	product.errors[:price].join('; ')
+		product.price = 1
+		assert product.invalid?
+	end
+
+	def new_product(image_url)
+		Product.new(title: 'Lorem Ipsum',
+								description: 'Lorem Ipsum Description',
+								image_url: image_url,
+								price: 1)
+	end
+
+	test "product image url must be correct format" do
+		# url изображения должен быть корректного формата
+		ok = %w{ andrew.gif andrew.jpg andrew.png ANDREW.JPG http://example.com/andrew.gif }
+		bad = %w{ andrew.doc andrew.gif/more andrew.gif.more }
+
+		ok.each do |img_url|
+			assert new_product(img_url).valid?, "#{img_url} shouldn't be invalid"
+		end
+
+		bad.each do |img_url|
+			assert new_product(img_url).invalid?, "#{img_url} shouldn't be valid"
+		end
+	end
+
+	test "product is not valid without a unique title" do
+		# товар должен быть с уникальным названием
+		product = Product.new(title: products(:kitty).title,
+													description: 'some_description',
+													image_url: 'some_image_url',
+													price: 1)
+		assert !product.save
+		assert_equal "has already been taken", product.errors[:title].join('; ')
+	end
 end
