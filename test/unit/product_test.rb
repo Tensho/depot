d66b35128673812ef124a72f0afa447e2e05d16a
Tensho <andrew.babichev@gmail.com>
2012-07-12 10:32:12 +0300

    Validation: title 10 characters the minimum allowed

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -25,7 +25,8 @@ class ProductTest < ActiveSupport::TestCase
 		assert product.invalid?
 		assert_equal "must be greater than or equal to 0.01",	product.errors[:price].join('; ')
 		product.price = 1
-		assert product.invalid?
+		assert product.valid?
+		# assert_equal "must be greater than or equal to 0.01",	product.errors[:price].join('; ')
 	end
 
 	def new_product(image_url)
@@ -57,5 +58,19 @@ class ProductTest < ActiveSupport::TestCase
 													price: 1)
 		assert !product.save
 		assert_equal "has already been taken", product.errors[:title].join('; ')
+	end
+
+	test "product title greater then or equal to 10 symbols" do
+		ok_product = Product.new(title: 'Lorem Ipsum',
+											description: 'Lorem Ipsum Description',
+											image_url: 'lorem.jpg',
+											price: 1)
+		assert ok_product.valid?
+		bad_product = Product.new(title: 'Lorem',
+											description: 'Lorem Ipsum Description',
+											image_url: 'lorem.jpg',
+											price: 1)
+		assert bad_product.invalid?
+		assert_equal "10 characters is the minimum allowed",	bad_product.errors[:title].join('; ')
 	end
 end
