70b0a4060dcd625e96e151b4a375fd31a4ddd8ba
Tensho <andrew.babichev@gmail.com>
2012-07-13 15:38:39 +0300

    'Cart'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true
@@ -7,4 +11,15 @@ class Product < ActiveRecord::Base
 		meassage: 'must be a URL for GIF, JPG or PNG image'
 	}
 	validates :title, :length => { :minimum => 10, :too_short => '%{count} characters is the minimum allowed' }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'line items exist')
+      return false
+    end
+  end
 end
